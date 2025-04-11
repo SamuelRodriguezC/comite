d1c80ed669c99b21e014c03ced23d2fa8fcf2d1d
Juanse <jmedinapineros@gmail.com>
2025-04-11 12:12:29 -0500

    Incorporación de paneles coordinator, evaluator, advisor y student

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdvisorPanelProvider::class,
+    App\Providers\Filament\CoordinatorPanelProvider::class,
+    App\Providers\Filament\EvaluatorPanelProvider::class,
+    App\Providers\Filament\StudentPanelProvider::class,
 ];
