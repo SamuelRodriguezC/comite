4e5043a71ff1b810f3c2158d430884a30c4dcfaa
Juanse <jmedinapineros@gmail.com>
2025-04-23 09:18:58 -0500

    Incorporación de recurso Transaction en panel advisor y evaluator

---
+++ app/Filament/Advisor/Resources/TransactionResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Advisor\Resources;
+
+use App\Filament\Advisor\Resources\TransactionResource\Pages;
+use App\Filament\Advisor\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('component')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Select::make('option_id')
+                    ->relationship('option', 'id')
+                    ->required(),
+                Forms\Components\TextInput::make('enabled')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('component')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('option.id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('enabled')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'view' => Pages\ViewTransaction::route('/{record}'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Evaluator/Resources/TransactionResource/Pages/ViewTransaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Evaluator\Resources\TransactionResource\Pages;
+
+use App\Filament\Evaluator\Resources\TransactionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewTransaction extends ViewRecord
+{
+    protected static string $resource = TransactionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
