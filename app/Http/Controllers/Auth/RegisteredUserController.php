b9b02f65cb9dbca6ba441517c4447ec863c8d658
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-14 11:59:51 -0500

    Ajustar Mensajes de Validación al registro  y corregir validación desde el front

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
@@ -32,16 +31,55 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'email' => ['required','string','email','max:100','unique:' . User::class,'bail'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults(), 'bail'],
             // Validaciones del perfil
-            'profile_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'document_id' => ['required', 'exists:documents,id'],
-            'document_number' => ['required', 'string', 'max:20', 'unique:profiles,document_number'],
-            'phone_number' => ['required', 'string', 'max:20'],
-            'level' => ['required', 'in:' . implode(',', array_column(\App\Enums\Level::cases(), 'value'))],
+            'profile_name' => ['required', 'string', 'max:50', 'bail'],
+            'last_name' => ['required', 'string', 'max:60', 'bail'],
+            'document_id' => ['required', 'exists:documents,id', 'bail'],
+            'document_number' => ['required', 'string', 'max:10', 'unique:profiles,document_number', 'min:7', 'bail'],
+            'phone_number' => ['required', 'string', 'max:10', 'bail'],
+            'level' => [
+                'required',
+                'in:' . implode(',', array_column(\App\Enums\Level::cases(), 'value')),
+                'bail'
+            ],
 
+        ], [
+            'email.required' => 'El campo correo electrónico es obligatorio.',
+            'email.string' => 'El campo correo electrónico debe ser una cadena de texto.',
+            'email.email' => 'El campo correo electrónico debe ser una dirección de correo electrónico válida.',
+            'email.max' => 'El campo correo electrónico no debe exceder los 100 caracteres.',
+            'email.unique' => 'El correo electrónico ya está registrado.',
+
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'password.confirmed' => 'La confirmación de la contraseña no coincide.',
+            'password.min' => 'La contraseña debe tener al menos 8 caracteres.', // Ejemplo de mensaje para min
+            'password.defaults' => 'La contraseña no cumple con los requisitos de seguridad.',
+
+            'profile_name.required' => 'El campo nombre es obligatorio.',
+            'profile_name.string' => 'El campo nombre debe ser una cadena de texto.',
+            'profile_name.max' => 'El campo nombre no debe exceder los 50 caracteres.',
+
+            'last_name.required' => 'El campo apellido es obligatorio.',
+            'last_name.string' => 'El campo apellido debe ser una cadena de texto.',
+            'last_name.max' => 'El campo apellido no debe exceder los 60 caracteres.',
+
+            'document_id.required' => 'El tipo de documento es obligatorio.',
+            'document_id.exists' => 'El tipo de documento seleccionado no es válido.',
+
+            'document_number.required' => 'El número de documento es obligatorio.',
+            'document_number.string' => 'El número de documento debe ser una cadena de texto.',
+            'document_number.max' => 'El número de documento no debe exceder los 10 caracteres.',
+            'document_number.min' => 'El número de documento no debe ser menor a los 7 caracteres.',
+            'document_number.unique' => 'El número de documento ya está en uso.',
+
+            'phone_number.required' => 'El número de teléfono es obligatorio.',
+            'phone_number.string' => 'El número de teléfono debe ser una cadena de texto.',
+            'phone_number.max' => 'El número de teléfono no debe exceder los 10 caracteres.',
+
+            'level.required' => 'El nivel es obligatorio.',
+            'level.in' => 'El nivel seleccionado no es válido.',
         ]);
 
         $user = User::create([
