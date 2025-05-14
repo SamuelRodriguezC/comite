5510b0aa4d8f9eb4d486b9d1cdd49ee45d28aa0f
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-14 09:06:38 -0500

    Corregir Estilos en el login y el registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,46 @@
 <x-guest-layout>
-    <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
-
-            <x-text-input id="password" class="block w-full mt-1"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+            <div class="relative">
+                <x-text-input id="password" class="block w-full mt-1"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+                <button type="button" id="togglePassword" class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-600 focus:outline-none">
+                    <x-heroicon-o-eye id="eyeIcon" class="w-6 h-6 text-red-800" />
+                    <x-heroicon-o-eye-slash id="eyeSlashIcon" class="hidden w-6 h-6 text-red-800" />
+                </button>
+            </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="text-indigo-600 border-gray-300 rounded shadow-sm dark:bg-gray-900 dark:border-gray-700 focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="text-sm text-gray-600 ms-2 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="text-red-600 border-red-500 rounded shadow-sm focus:ring-gray-300" name="remember">
+                <span class="text-sm text-gray-500 ms-2">{{ __('Recordarme') }}</span>
             </label>
         </div>
 
-        <!-- incorporación de botones para recuperar contraseña y resgistrarse -->
         <div class="flex justify-between my-5">
-            <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
-                {{ __('¿Olvidó su cuntraseña?') }}
+            <a class="text-sm text-gray-500 underline rounded-md hover:text-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-700" href="{{ route('password.request') }}" wire:navigate>
+                {{ __('¿Olvidó su Contraseña?') }}
             </a>
-            <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}" wire:navigate>
+            <a class="text-sm text-gray-500 underline rounded-md 0 hover:text-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-700" href="{{ route('register') }}" wire:navigate>
                 {{ __('Registrarse') }}
             </a>
         </div>
 
-        <!-- iniciar sesion -->
         <div class="flex items-center justify-center mt-4">
             <x-primary-button class="justify-center w-full">
                 {{ __('Iniciar sesión') }}
@@ -50,3 +48,30 @@
         </div>
     </form>
 </x-guest-layout>
+
+<script>
+    // Obtiene una referencia al botón que se usará para mostrar/ocultar la contraseña
+    const togglePasswordButton = document.getElementById('togglePassword');
+
+    const passwordInput = document.getElementById('password');
+
+    const eyeIcon = document.getElementById('eyeIcon');
+
+    const eyeSlashIcon = document.getElementById('eyeSlashIcon');
+
+    // Agrega un "escuchador de eventos" al botón. Cuando se haga clic en él, la función dentro se ejecutará.
+    togglePasswordButton.addEventListener('click', () => {
+        // Si es 'password', lo cambia a 'text' para mostrar la contraseña.
+        // Si no es 'password' (es decir, es 'text'), lo cambia a 'password' para ocultarla.
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        // Establece el atributo 'type' del campo de entrada de la contraseña al nuevo valor ('text' o 'password').
+        passwordInput.setAttribute('type', type);
+
+        // Esto hace que el icono del ojo se muestre u oculte.
+        eyeIcon.classList.toggle('hidden');
+
+        // Esto asegura que solo el icono apropiado (mostrar u ocultar contraseña) esté visible.
+        eyeSlashIcon.classList.toggle('hidden');
+    });
+</script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,63 +2,67 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
 
-            <x-text-input id="password" class="block w-full mt-1"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <div class="relative">
+                <x-text-input id="password" class="block w-full mt-1"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="new-password" />
+                <button type="button" id="togglePasswordMain" class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-600 focus:outline-none">
+                    <x-heroicon-o-eye id="eyeIconMain" class="w-6 h-6 text-red-800" />
+                    <x-heroicon-o-eye-slash id="eyeSlashIconMain" class="hidden w-6 h-6 text-red-800" />
+                </button>
+            </div>
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirmación de Contraseña')" />
 
-            <x-text-input id="password_confirmation" class="block w-full mt-1"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
+            <div class="relative">
+                <x-text-input id="password_confirmation" class="block w-full mt-1"
+                                    type="password"
+                                    name="password_confirmation" required autocomplete="new-password" />
+                <button type="button" id="togglePassword" class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-600 focus:outline-none">
+                    <x-heroicon-o-eye id="eyeIcon" class="w-6 h-6 text-red-800" />
+                    <x-heroicon-o-eye-slash id="eyeSlashIcon" class="hidden w-6 h-6 text-red-800" />
+                </button>
+            </div>
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         {{-- -----------Información De Perfil ----------- --}}
 
-        <!-- Name -->
         <div class="mt-4">
             <x-input-label for="profile_name" :value="__('Nombre de Perfil')" />
             <x-text-input id="profile_name" class="block w-full mt-1" type="text" name="profile_name" :value="old('profile_name')" required />
             <x-input-error :messages="$errors->get('profile_name')" class="mt-2" />
         </div>
 
-        <!-- Last Name -->
         <div class="mt-4">
             <x-input-label for="last_name" :value="__('Apellidos')" />
             <x-text-input id="last_name" class="block w-full mt-1" type="text" name="last_name" :value="old('last_name')" required />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
         </div>
 
-        <!-- Document Type -->
         <div class="mt-4">
             <x-input-label for="document_id" :value="__('Tipo de Documento')" />
-            <select id="document_id" name="document_id" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+            <select id="document_id" name="document_id" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-red-500 focus:ring-red-500">
                 <option value="">Seleccione una opción</option>
                 @foreach ($documents as $document)
                     <option value="{{ $document->id }}" {{ old('document_id') == $document->id ? 'selected' : '' }}>
@@ -69,41 +73,35 @@
             <x-input-error :messages="$errors->get('document_id')" class="mt-2" />
         </div>
 
-        <!-- Document Number -->
         <div class="mt-4">
             <x-input-label for="document_number" :value="__('Número de Documento')" />
-            <x-text-input id="document_number" class="block w-full mt-1" type="text" name="document_number" :value="old('document_number')" required />
+            <x-text-input id="document_number" class="block w-full mt-1" type="number" name="document_number" :value="old('document_number')" required />
             <x-input-error :messages="$errors->get('document_number')" class="mt-2" />
         </div>
 
-        <!-- Phone Number -->
         <div class="mt-4">
             <x-input-label for="phone_number" :value="__('Número de Telefono')" />
-            <x-text-input id="phone_number" class="block w-full mt-1" type="text" name="phone_number" :value="old('phone_number')" required />
+            <x-text-input id="phone_number" class="block w-full mt-1" type="number" name="phone_number" :value="old('phone_number')" required />
             <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
         </div>
 
-        <!-- Level -->
         <div class="mt-4">
             <x-input-label for="level" :value="__('Nivel Universitario')" />
-            <select id="level" name="level" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-                @foreach (\App\Enums\Level::cases() as $level)
-                    <option value="{{ $level->value }}" {{ old('level') == $level->value ? 'selected' : '' }}>
-                        {{ $level->getLabel() }}
-                    </option>
-                @endforeach
+            <select id="level" name="level" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-red-500 focus:ring-red-500">
+                    <option disabled selected>Seleccionar</option>
+                    <option value="1">Pregrado</option>
+                    <option value="2">Posgrado</option>
             </select>
             <x-input-error :messages="$errors->get('level')" class="mt-2" />
         </div>
 
-        <!-- incorporar inicio de sesion y recuperación de contraseña -->
         <div class="flex justify-between my-5">
-            <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
-                {{ __('Iniciar sesión') }}
+            <a class="text-sm text-gray-500 underline rounded-md hover:text-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-700" href="{{ route('login') }}" wire:navigate>
+                {{ __('Iniciar Sesión') }}
             </a>
 
-            <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
-                {{ __('¿Olvidó su contraseña?') }}
+            <a class="text-sm text-gray-500 underline rounded-md hover:text-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-700" href="{{ route('password.request') }}" wire:navigate>
+                {{ __('¿Olvidó su Contraseña?') }}
             </a>
         </div>
 
@@ -114,3 +112,34 @@
         </div>
     </form>
 </x-guest-layout>
+<script>
+    // Obtiene referencias al botón y al input de la confirmación de contraseña
+    const togglePasswordConfirmationButton = document.getElementById('togglePassword');
+    const passwordConfirmationInput = document.getElementById('password_confirmation');
+    const eyeIconConfirmation = document.getElementById('eyeIcon');
+    const eyeSlashIconConfirmation = document.getElementById('eyeSlashIcon');
+
+    // Obtiene referencias al botón y al input de la contraseña principal
+    const togglePasswordButtonMain = document.getElementById('togglePasswordMain');
+    const passwordInput = document.getElementById('password');
+    const eyeIconMain = document.getElementById('eyeIconMain');
+    const eyeSlashIconMain = document.getElementById('eyeSlashIconMain');
+
+    // Función para mostrar/ocultar la contraseña
+    function togglePasswordVisibility(inputElement, eyeIconElement, eyeSlashIconElement) {
+        const type = inputElement.getAttribute('type') === 'password' ? 'text' : 'password';
+        inputElement.setAttribute('type', type);
+        eyeIconElement.classList.toggle('hidden');
+        eyeSlashIconElement.classList.toggle('hidden');
+    }
+
+    // Agrega un "escuchador de eventos" al botón de la confirmación de contraseña
+    togglePasswordConfirmationButton.addEventListener('click', () => {
+        togglePasswordVisibility(passwordConfirmationInput, eyeIconConfirmation, eyeSlashIconConfirmation);
+    });
+
+    // Agrega un "escuchador de eventos" al botón de la contraseña principal
+    togglePasswordButtonMain.addEventListener('click', () => {
+        togglePasswordVisibility(passwordInput, eyeIconMain, eyeSlashIconMain);
+    });
+</script>
