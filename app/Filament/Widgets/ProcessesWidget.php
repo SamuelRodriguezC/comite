9959f7b7b8e573993bd8ce5e27003d1ea5eab96c
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-16 16:59:25 -0500

    Widgets con graficas en el panel de coordinador

--- app/Filament/Widgets/ProcessesWidget.php
+++ app/Filament/Widgets/ProcessesWidget.php
@@ -9,6 +9,7 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class ProcessesWidget extends BaseWidget
 {
+    protected static ?int $sort = 1;
     protected function getStats(): array
     {
         return [
