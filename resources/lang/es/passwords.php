19ec349c4773779f9e53e385d0b8f0c4d0124f3f
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-16 15:57:49 -0500

    Crear traductor de validación para contraseñas

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo el enlace para restablecer la contraseña!',
+    'throttled' => 'Por favor espera antes de intentar nuevamente.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => "No pudimos encontrar un usuario con ese correo electrónico.",
+];
