3fa89ec947a9f20ad73f029dfbd6cba236dc662a
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-27 10:40:20 -0500

    Configurar comando para actualizar estado de los procesos a "vencido" cuando sea pertinente

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,35 @@
 <?php
 
+use Carbon\Carbon;
+use App\Enums\State;
+use App\Models\Process;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+Artisan::command('processes:update-expired', function () {
+    $now = Carbon::now();
+
+    $expiredProcesses = Process::where('delivery_date', '<', $now)
+        ->where('state', '!=', State::VENCIDO->value)
+        ->get();
+
+    foreach ($expiredProcesses as $process) {
+        $process->state = State::VENCIDO->value;
+        $process->save();
+    }
+
+    $this->info("Updated {$expiredProcesses->count()} processes to VENCIDO.");
+})->describe('Update the state of processes to VENCIDO if delivery_date is past due');
+
+// Programar la tarea periódica
+app()->booted(function () {
+    $schedule = app(Schedule::class);
+    $schedule->command('processes:update-expired')->everySecond();
+});
