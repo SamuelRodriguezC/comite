a146a771d2458f62dc9a8f4a760736eecdf50b31
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-27 11:44:42 -0500

    Ajuste en widget de coordinador

--- app/Filament/Widgets/ProcessState.php
+++ app/Filament/Widgets/ProcessState.php
@@ -49,7 +49,9 @@ class ProcessState extends PieChartWidget
                         'rgba(255, 159, 64, 0.6)',
                         'rgba(201, 203, 207, 0.6)',
                         'rgba(255, 99, 132, 0.6)',
-                        'rgba(54, 162, 235, 0.6)'
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 0, 0, 0.6)',
+
                     ],
                     'borderColor' => [
                         'rgb(75, 192, 192)',
@@ -57,7 +59,9 @@ class ProcessState extends PieChartWidget
                         'rgb(255, 159, 64)',
                         'rgb(201, 203, 207)',
                         'rgba(255, 99, 132)',
-                        'rgb(54, 162, 235)'
+                        'rgb(54, 162, 235)',
+                        'rgba(255, 0, 0, 0.78)',
+
                     ],
                     'borderWidth' => 1
                 ],
