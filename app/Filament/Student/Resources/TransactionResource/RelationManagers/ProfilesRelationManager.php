83ea928911c25e2efe73293fb0333e8442157205
SamuelRodriguezC <samuelrc250@gmail.com>
2025-05-30 09:57:22 -0500

    Validación para estudaintes en los perfiles de la transacción (solo pueden vincular un estudiante y un asesor)

--- app/Filament/Student/Resources/TransactionResource/RelationManagers/ProfilesRelationManager.php
+++ app/Filament/Student/Resources/TransactionResource/RelationManagers/ProfilesRelationManager.php
@@ -31,7 +31,7 @@ class ProfilesRelationManager extends RelationManager
     protected static string $relationship = 'profiles';
     protected static ?string $title = 'Integrante(s) vinculados a la Opción';
 
-    // ---------- OPTENER LA TRANSACCIÓN A LA QUE PERTENECEN LOS PERFILES ----------
+    // ---------- OBTENER LA TRANSACCIÓN A LA QUE PERTENECEN LOS PERFILES ----------
     protected function getTransaction(): \App\Models\Transaction
     {
         return $this->ownerRecord; // $this->ownerRecord es el modelo Transaction al que pertenece este RelationManager
@@ -50,31 +50,9 @@ class ProfilesRelationManager extends RelationManager
     public function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Forms\Components\TextInput::make('document_number')
-                ->required()
-                ->maxLength(255)
-                ->visibleOn('create'),
-            Select::make('courses_id')
-                ->label('Curso')
-                ->options(function (Get $get) {
-                    $documentNumber = $get('document_number');
-                    if (!$documentNumber) {
-                        return [];
-                    }
-                    // Buscar el perfil usando el número de documento
-                    $profile = \App\Models\Profile::where('document_number', $documentNumber)->first();
-                    if (!$profile) {
-                        return [];
-                    }
-                    $userLevel = $profile->level;
-                    return \App\Models\Course::where('level', $userLevel)
-                        ->pluck('course', 'id');
-                })
-                ->searchable()
-                ->required()
-                ->columnSpanFull(),
-        ]);
+            ->schema([
+                //
+            ]);
     }
 
     public function table(Table $table): Table
@@ -101,7 +79,7 @@ class ProfilesRelationManager extends RelationManager
                     // Transformar el ID del curso a su nombre
                     ->formatStateUsing(function ($state) {
                         return \App\Models\Course::find($state)?->course ?? 'Curso no encontrado';
-                }),
+                    }),
                 Tables\Columns\TextColumn::make('pivot.role_id')
                     ->label('Rol')
                     // Transformar el ID del curso a su nombre
@@ -115,63 +93,83 @@ class ProfilesRelationManager extends RelationManager
             // No se puede filtrar estudiantes con mi mismo nivel universitario, porque el método getRecordSelect está en la carpeta vendor
             ->headerActions([
                 Tables\Actions\AttachAction::make()
-                ->modalHeading('Ingrese el número del documento de identidad de la persona que quiere vincular')
-                ->form(fn (AttachAction $action): array => [
-                    $action->getRecordSelect()
-                        ->reactive(), // Necesario para que al seleccionar cambien las carreras
-                    Select::make('courses_id')
-                        ->label('Ingrese la carrera de la persona vinculada')
-                        ->options(function (Get $get) {
-                            $recordId = $get('recordId'); // 'recordId' es el ID de la persona seleccionada
-                            if (!$recordId) {
-                                return [];
-                            }
-                            $profile = \App\Models\Profile::find($recordId);
-                            if (!$profile) {
-                                return [];
-                            }
-                            return getCoursesByProfileLevel($profile->level);
-                        })
-                        ->searchable()
-                        ->required(),
-                    Select::make('role_id')
-                        ->label('Función del integrante')
-                        ->options(function (Get $get) {
-                            $recordId = $get('recordId');
-                            if (!$recordId) return ["No hay perfil seleccionado"];
-                            $profile = \App\Models\Profile::find($recordId);
-                            if (!$profile || !$profile->user) return ["El perfil no existe o no tiene usuario asociado"];
-                            // Retorna los roles como array
-                            return $profile->user->roles->pluck('name', 'id');
-                        })
-                        ->searchable()
-                        ->required(),
-                ])->visible(fn () => $this->getTransaction()->isEditable()) //Solo puede vincular personas antes del tiempo determinado
+                    ->modalHeading('Ingrese el número del documento de identidad de la persona que quiere vincular')
+                    ->modalDescription('Solo podrá vincular integrantes dentro de las 12 horas siguientes a la creación de la Opción. Durante este periodo, únicamente será posible agregar un estudiante adicional y un asesor. Los perfiles vinculados serán posteriormente revisados por el coordinador y se asignará comité evaluador si la opción lo requiere.')
+                    ->form(fn(AttachAction $action): array => [
+                        $action->getRecordSelect()
+                            ->label('Perfil')
+                            ->reactive()
+                            ->options(function () {
+                                return \App\Models\Profile::query()
+                                    // Filtra según tu criterio, por ejemplo, sin rol evaluador:
+                                    ->whereDoesntHave('user.roles', function ($q) {
+                                        $q->where('name', 'Evaluador');
+                                    })
+                                    ->get()
+                                    ->mapWithKeys(function ($profile) {
+                                        return [$profile->id => $profile->document_number . ' - ' . $profile->name . ' ' . $profile->last_name,];
+                                    });
+                            })
+                            ->afterStateUpdated(function (Set $set) {
+                                // Limpiar los campos al cambiar el perfil
+                                $set('courses_id', null);
+                                $set('role_id', null);
+                            }),
+
+                        Select::make('courses_id')
+                            ->label('Ingrese la carrera de la persona vinculada')
+                            ->options(function (Get $get) {
+                                $recordId = $get('recordId');
+                                if (!$recordId) return [];
+                                $profile = \App\Models\Profile::find($recordId);
+                                if (!$profile) return [];
+                                return getCoursesByProfileLevel($profile->level);
+                            })
+                            ->required(),
+                        Select::make('role_id')
+                            ->label('Función del integrante')
+                            ->options(function (Get $get) {
+                                $recordId = $get('recordId');
+                                if (!$recordId) return ["No hay perfil seleccionado"];
+                                $profile = \App\Models\Profile::find($recordId);
+                                if (!$profile || !$profile->user) return ["El perfil no existe o no tiene usuario asociado"];
+                                return $profile->user->roles->pluck('name', 'id');
+                            })
+                            ->required()
+                              ->rule(function (Get $get) {
+                                return function ($attribute, $value, $fail) use ($get) {
+                                    $transaction = $this->getTransaction();
+
+                                    $studentRoleId = \App\Models\Role::where('name', 'Estudiante')->first()?->id;
+                                    $advisorRoleId = \App\Models\Role::where('name', 'Asesor')->first()?->id;
+                                    $evaluatorRoleId = \App\Models\Role::where('name', 'Evaluador')->first()?->id;
+
+                                    $profiles = $transaction->profiles()->withPivot('role_id')->get();
+
+                                    $studentCount = $profiles->where('pivot.role_id', $studentRoleId)->count();
+                                    $advisorCount = $profiles->where('pivot.role_id', $advisorRoleId)->count();
+                                    $evaluatorCount = $profiles->where('pivot.role_id', $evaluatorRoleId)->count();
+
+                                    if ($value == $studentRoleId && $studentCount >= 2) {
+                                        $fail('Ya hay 2 estudiantes vinculados a esta transacción.');
+                                    }
+
+                                    if ($value == $advisorRoleId && $advisorCount >= 1) {
+                                        $fail('Ya hay 1 asesor vinculado a esta transacción.');
+                                    }
+
+                                    if ($value == $evaluatorRoleId && $evaluatorCount >= 0) {
+                                        $fail('Usted no tiene permisos para asignar Evaluadores');
+                                    }
+                                };
+                            }),
+                    ])->attachAnother(false)
+                    ->visible(fn() => $this->getTransaction()->isEditable() && $this->canAttachMoreProfiles())
+
             ])
             ->actions([
-                // // Botón para ver detalles de integrante
-                // Tables\Actions\ViewAction::make()
-                // ->label('Ver')
-                // ->modalHeading('Información del Integrante')
-                // // Crear el modal con una infolista
-                // ->infolist(function ($record){
-                //     return [
-                //         Section::make('Información Personal')
-                //         ->schema([
-                //             TextEntry::make('name')->label('Nombre'),
-                //             TextEntry::make('last_name')->label('Apellido'),
-                //             TextEntry::make('User.email')->label('Email'),
-                //             TextEntry::make('phone_number')->label('Número de Teléfono'),
-                //         ])->columns(2)->columnSpan(2),
-                //     //     Section::make([
-                //     //         extEntry::make('level')->label('Nivel Universitario')->formatStateUsing(fn ($state) => Level::from($state)->getLabel()),
-                //     //     ])->columnSpan(1)
-                //     // ])->columns(3)
-                //     ->record($record)),// El $record aquí viene del modelo actual en la tabla
 
-                //     ];
-                // }),
-                // --------------------------- VER PROCESO ---------------------------
+                // --------------------------- VER DETALLES ---------------------------
                 Tables\Actions\ViewAction::make()
                     ->label('Ver')
                     ->infolist(function ($record) {
@@ -186,43 +184,65 @@ class ProfilesRelationManager extends RelationManager
                                         ->label('Email'),
                                     TextEntry::make('phone_number')
                                         ->label('Número de Teléfono'),
-                                ]) ->columns(1),  // Esto asegura que cada sección ocupe una columna
+                                ])->columns(1),
 
                             Section::make('Información Institucional')
                                 ->schema([
                                     TextEntry::make('level')
                                         ->label('Nivel Universitario')
-                                        ->formatStateUsing(fn ($state) => Level::from($state)->getLabel()),
+                                        ->formatStateUsing(fn($state) => Level::from($state)->getLabel()),
                                     TextEntry::make('pivot.courses_id')
                                         ->label('Carrera')
-                                        ->formatStateUsing(function ($state) {return \App\Models\Course::find($state)?->course ?? 'Curso no encontrado';}),
+                                        ->formatStateUsing(function ($state) {
+                                            return \App\Models\Course::find($state)?->course ?? 'Curso no encontrado';
+                                        }),
                                     TextEntry::make('pivot.role_id')
                                         ->label('Rol')
-                                        ->formatStateUsing(function ($state) {return \App\Models\Role::find($state)?->name ?? 'Rol no encontrado';}),
-                            ])->columns(1),  // Esto asegura que cada sección ocupe una columna
+                                        ->formatStateUsing(function ($state) {
+                                            return \App\Models\Role::find($state)?->name ?? 'Rol no encontrado';
+                                        }),
+                                ])->columns(1),
                         ];
                     }),
 
                 // Solo la persona en sesión puede cambiar su carrera y editarla antes del tiempo determinado
                 Tables\Actions\EditAction::make()
-                        ->visible(fn ($record) =>
+                    ->visible(
+                        fn($record) =>
                         $record->id === auth_profile_id() &&
-                        $this->getTransaction()->isEditable()
+                            $this->getTransaction()->isEditable()
                     ),
                 // La persona en sesión no puede desvincularse y puede desvincular a otros antes del tiempo determinado
                 Tables\Actions\DetachAction::make()
-                        ->visible(fn ($record) =>
+                    ->visible(
+                        fn($record) =>
                         $record->id !== auth_profile_id() &&
-                        $this->getTransaction()->isEditable()
+                            $this->getTransaction()->isEditable()
                     ),
             ])
             ->emptyStateActions([
                 Tables\Actions\AttachAction::make(),
             ])
             ->bulkActions([
-                    Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\BulkActionGroup::make([]),
+            ]);
+    }
 
-                    ]),
-                ]);
+
+    // Función Auxiliar para determinar si el estudiante puede vincular mas personas
+    protected function canAttachMoreProfiles(): bool
+    {
+        $transaction = $this->getTransaction();
+
+        // Cuenta los perfiles existentes con roles específicos
+        $profiles = $transaction->profiles()->withPivot('role_id')->get();
+
+        $studentRoleId = \App\Models\Role::where('id', 1)->first()?->id;
+        $advisorRoleId = \App\Models\Role::where('id', 2)->first()?->id;
+
+        $studentCount = $profiles->where('pivot.role_id', $studentRoleId)->count();
+        $advisorCount = $profiles->where('pivot.role_id', $advisorRoleId)->count();
+
+        return $studentCount < 2 || $advisorCount < 1;
     }
 }
